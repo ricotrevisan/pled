91992b9ff02efea49b411b65578401f6443fd667
Rico <r@rico.wtf>
2026-06-03 22:28:44 +0200

    Bump version from 0.0.28-beta to 0.0.29-beta

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pled.MixProject do
   def project do
     [
       app: :pled,
-      version: "0.0.28-beta",
+      version: "0.0.29-beta",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
