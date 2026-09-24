c1dbc7a169822668201686a58bc0dc2eaf441d57
Rico Trevisan <r@rico.wtf>
2026-09-24 11:20:55 +0200

    Bump version from 0.1.1 to 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pled.MixProject do
   def project do
     [
       app: :pled,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.20",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
